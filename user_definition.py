a1dfa6915e68b0413eb85bedc94ee90e92d6268a
Brian Chivers <bpchivers@usfca.edu>
2019-03-26 17:50:33 -0700

    add hw1 files

---
+++ user_definition.py
@@ -0,0 +1,9 @@
+ec2_address = "ec2-34-219-67-172.us-west-2.compute.amazonaws.com"
+user = "ec2-user"
+key_file = "/.ssh/bpchivers_oregon.pem"
+git_repo_owner = "MSDS698"
+git_repo_name = "whatrthose"
+git_user_id = "bpchivers"
+orig_coord = '37.7909,-122.3925'
+dest_coord = '37.7765,-122.4506'
+output_file_name = 'output.txt'
