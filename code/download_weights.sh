024bd4826d59121275fa0aea6a6ae81af4df8dfb
benjamin khuong <bkhuong@dons.usfca.edu>
2019-05-03 13:29:16 -0700

    updated environment.yml for detection model

--- code/download_weights.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-filename="app/models/frcnn_detector/weights/model_frcnn_vgg.hdf5"
-file_id="1KDS9XREi7-cM-ibGML0GLoekCzCkZR1z"
-query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${file_id}" \
-| perl -nE'say/uc-download-link.*? href="(.*?)\">/' \
-| sed -e 's/amp;//g' | sed -n 2p`
-url="https://drive.google.com$query"
-curl -b ./cookie.txt -L -o ${filename} $url
